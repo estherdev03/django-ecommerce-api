22dd3c6fdfd0e80193d466bca2d9cbcef7dd0319
Esther <esthertrandev@gmail.com>
2026-03-23 19:16:25 -0600

    feat: add url endpoint for filtering products by category

--- ecommerce/product/views.py
+++ ecommerce/product/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.response import Response
 from drf_spectacular.utils import extend_schema
+from rest_framework.decorators import action
 
 from .models import Category, Brand, Product
 from .serializers import CategorySerializer, BrandSerializer, ProductSerializer
@@ -38,5 +39,18 @@ class ProductView(viewsets.ViewSet):
 
     @extend_schema(responses=ProductSerializer)
     def list(self, request):
+        """An endpoint to return a list of all products"""
         serializer = ProductSerializer(self.queryset, many=True)
+        return Response(serializer.data)
+
+    @action(
+        methods=["get"],
+        detail=False,
+        url_path=r"category/(?P<category>\w+)/all",
+    )
+    def list_product_by_category(self, request, category=None):
+        """An endpoint to return products by category"""
+        serializer = ProductSerializer(
+            self.queryset.filter(category__name=category), many=True
+        )
         return Response(serializer.data)
