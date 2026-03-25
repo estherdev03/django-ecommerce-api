d162106500d36ba630aec1836b52ad8fdda5f65a
Esther <esthertrandev@gmail.com>
2026-03-24 21:15:00 -0600

    perf: optimize number of queries in retrieve method

--- ecommerce/product/views.py
+++ ecommerce/product/views.py
@@ -3,6 +3,12 @@ from rest_framework import viewsets
 from rest_framework.response import Response
 from drf_spectacular.utils import extend_schema
 from rest_framework.decorators import action
+from django.db import connection
+
+# from pygments import highlight
+# from pygments.formatters import TerminalFormatter
+# from pygments.lexers import SqlLexer
+# from sqlparse import format
 
 from .models import Category, Brand, Product
 from .serializers import CategorySerializer, BrandSerializer, ProductSerializer
@@ -36,12 +42,23 @@ class ProductView(viewsets.ViewSet):
     """A simple Viewset for viewing all products"""
 
     queryset = Product.objects.all()
+
     lookup_field = "slug"
 
     def retrieve(self, request, slug=None):
-        serializer = ProductSerializer(self.queryset.filter(slug=slug), many=True)
-
-        return Response(serializer.data)
+        serializer = ProductSerializer(
+            self.queryset.filter(slug=slug).select_related("category", "brand"),
+            many=True,
+        )
+        data = Response(serializer.data)
+        # --------------Analyze number of queries in this method----------------------
+        # q = list(connection.queries)
+        # for qs in q:
+        #     sqlformatted = format(str(qs["sql"]), reindent=True)
+        #     print(highlight(sqlformatted, SqlLexer(), TerminalFormatter()))
+        # print(len(q))
+        # -----------------------------------------------------------------------------
+        return data
 
     @extend_schema(responses=ProductSerializer)
     def list(self, request):
