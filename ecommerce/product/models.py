a45fbd20ed7bffc942e26c8f6c2874913a8cb62c
Esther <esthertrandev@gmail.com>
2026-03-25 13:49:26 -0600

    feat: add ProductImage model

--- ecommerce/product/models.py
+++ ecommerce/product/models.py
@@ -73,3 +73,27 @@ class ProductLine(models.Model):
 
     def __str__(self):
         return str(self.sku)
+
+
+class ProductImage(models.Model):
+    name = models.CharField(max_length=100)
+    alternative_text = models.CharField(max_length=100)
+    url = models.ImageField(upload_to=None)
+    product_line = models.ForeignKey(
+        ProductLine, on_delete=models.CASCADE, related_name="product_image"
+    )
+    order = OrderField(unique_for_field="product_line", blank=True)
+
+    def clean(self):
+        qs = ProductImage.objects.filter(product_line=self.product_line)
+        for obj in qs:
+            if self.id != obj.id and self.order == obj.order:
+                raise ValidationError("Duplicate value.")
+
+    # Auto initiate clean up when save to db
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        return super(ProductImage, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return str(self.name)
